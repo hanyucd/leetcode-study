47b0cf4af516159a67735b72c4f4078abc8eb953
hanyucd <1807003141@qq.com>
2020-12-07 23:23:32 +0800

    反转字符串中的单词

--- code/string/string-1.js
+++ code/string/string-1.js
@@ -6,7 +6,6 @@
     输出："s'teL ekat edoCteeL tsetnoc"
  */
 
-// 方法 1
 const reverseWords = str => {
   let strAry = str.split(' '); // 字符串按空格进行分隔返回数组
   let reverseAry = strAry.map(item => { 
@@ -20,6 +19,24 @@ const reverseWords = str => {
 const rawStr = "Let's take LeetCode contest";
 const result = reverseWords(rawStr);
 
-console.log('rawStr:', rawStr);
-console.log('result:', result);
+// console.log('rawStr:', rawStr);
+// console.log('result:', result);
 
+/**
+ * 翻转单词顺序
+ * 
+ * 示例：
+ *  输入： 'I am a student.'
+ *  输出： 'student. a am I'
+ */
+
+ const reverseSentence = str => {
+   if(!str) return '';
+   return str.split(' ').reverse().join(' ');
+ };
+
+const rawStr2 = "I am a student.";
+const result2 = reverseSentence(rawStr2);
+
+console.log('rawStr2:', rawStr2);
+console.log('result2:', result2);
